266abc102ef5d195ce31e9f16d147d21bdad1b58
Utku <utkuulasaltin@gmail.com>
2022-11-03 17:09:30 +0300

    added bottom navigation bar

--- app/src/main/java/com/utkuulasaltin/bluevalleyshop/MainActivity.kt
+++ app/src/main/java/com/utkuulasaltin/bluevalleyshop/MainActivity.kt
@@ -3,13 +3,18 @@ package com.utkuulasaltin.bluevalleyshop
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.utkuulasaltin.bluevalleyshop.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         initNavController()
     }
 
@@ -17,5 +22,7 @@ class MainActivity : AppCompatActivity() {
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
+        binding.bottomNavigationView.setupWithNavController(navController)
+        binding.isVisibleBar = navController.currentDestination?.id == R.id.loginFragment
     }
 }
